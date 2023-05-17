0f650fcd97d4cd49813e1a51e55ef468395145b0
povconfluent <pov.confluent@gmail.com>
2023-05-17 16:22:07 +0200

    cambios en archivos

--- course/example_pipelineAdult.py
+++ course/example_pipelineAdult.py
@@ -6,11 +6,14 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("PipelineExample").getOrCreate()
 dataset = spark.read.option("header", "true").option("inferSchema", "true").csv("file:///course/datasets/adult.csv")# Prepare training documents from a list of (id, text, label) tuples.
 
+dataset.show(10)
+
+
 trainDF, testDF = dataset.randomSplit([0.8, 0.2], seed=42)
 trainDF.cache().count() # Cache because accessing training data multiple times, el acceso es más rápido. 
 
 
-categoricalCols = ["workclass", "education", "marital_status", "occupation", "relationship", "race", "sex"]
+categoricalCols = ["workclass", "education", "marital-status", "occupation", "relationship", "race", "gender"]
 
 # The following two lines are transformers. They return functions that we will later apply to transform the dataset.
 stringIndexer = StringIndexer(inputCols=categoricalCols, outputCols=[x + "Index" for x in categoricalCols]) 
@@ -23,7 +26,7 @@ encoder = OneHotEncoder(inputCols=stringIndexer.getOutputCols(), outputCols=[x +
 labelToIndex = StringIndexer(inputCol="income", outputCol="label")
 
 # This includes both the numeric columns and the one-hot encoded binary vector columns in our dataset.
-numericCols = ["age", "fnlwgt", "education_num", "capital_gain", "capital_loss", "hours_per_week"]
+numericCols = ["age", "fnlwgt", "educational-num", "capital-gain", "capital-loss", "hours-per-week"]
 assemblerInputs = [c + "OHE" for c in categoricalCols] + numericCols
 #is important to mention that spark models recieve a vector (unique column with values)
 vecAssembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
@@ -47,3 +50,4 @@ for row in selected.collect():
             f, l, p
         )
     )
+pipelineModel.save("file:///course/datasets/model.pkl")
